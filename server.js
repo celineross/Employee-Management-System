3a491a4c878f360728f419f34255183a3e433d6f
Celine Ross <celineross@gmail.com>
2020-10-13 12:08:45 -0500

    added skeleton for switch case statements

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
 var inquirer = require("inquirer");
 var ct = require("console.table");
-require("dotenv").config();
+const connection = require("./connection");
 
+async function start() {
+    inquirer.prompt({
+        type: "list",
+        message: "Choose an option below: ",
+        name: "options",
+        choices: [
+            "View employees",
+            "View departments",
+            "View roles",
+            "Add employee",
+            "Add department",
+            "Add role",
+            "Exit"
+        ]
+    })
+    .then(function(result) {
+        switch (result.options) {
+            // case "View employees":
+            // case "View departments":
+            // case "View roles":
+            // case "Add employee":
+            // case "Add department":
+            // case "Add role":
+            case "Exit":
+            connection.end();
+            break;
+        }
+    })
+}
