f3e3022a0e35c1910af0ef65a8a7c5f5db3f7105
Celine Ross <celineross@gmail.com>
2020-10-13 11:21:07 -0500

    added schema seeds

---
+++ seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO 
+    department (department)
+VALUES
+    ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal");
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ("Accountant", 80000, 3);
+
+INSERT INTO
+    employee (first_name, last_name)
+VALUES
+    ("Angela", "Martin"),
+    ("Oscar", "Martinez"),
+    ("Kevin", "Malone");
