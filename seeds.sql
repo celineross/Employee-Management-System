13b0d39bef49940df72ad8c50ab3d3d1aa2de9f2
Celine Ross <celineross@gmail.com>
2020-10-13 12:50:54 -0500

    changing around schema

--- seeds.sql
+++ seeds.sql
@@ -4,16 +4,19 @@ VALUES
     ("Sales"),
     ("Engineering"),
     ("Finance"),
-    ("Legal");
+    ("Support");
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
+    ("Head Accountant", 90000, 1)
+    ("Salesman", 40000, 2),
     ("Accountant", 80000, 3);
+    ("Customer Service Rep", 35000, 4)
 
 INSERT INTO
-    employee (first_name, last_name)
+    employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ("Angela", "Martin"),
-    ("Oscar", "Martinez"),
-    ("Kevin", "Malone");
+    ("Angela", "Martin", 3, 1),
+    ("Oscar", "Martinez", 3, null),
+    ("Kevin", "Malone", 3, null);
